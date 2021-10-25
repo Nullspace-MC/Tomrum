689ad4a7c3a06ec849258716847bc0f634f8b26c
nessie <nessie@hotmail.dk>
2021-10-25 15:57:20 +0200

    Version 0.1.2: * Make sides of sticky pistons sticky.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Adds some useful features for mainly creative dev:
 * Fix for scoreboard sidebar hiding when there are more than 15 players on the scoreboard.
 * Fullbright setting by sliding brightness slider to max.
 * Increased chat limit (8x length, i.e., 800 chars).
+* Mostly working sticky piston sides à la VanillaTweaks.
 * Creative Mode sprinting while flying.
 * Creative Mode noclip (toggleable).
 * Creative Mode flight nodrift/flight inertia cancellation (toggleable).
---
+++ src/main/java/net/dugged/tomrum/mixins/CMixinStickyPistonSides.java
@@ -0,0 +1,52 @@
+package net.dugged.tomrum.mixins;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPistonBase;
+import net.minecraft.block.BlockPistonExtension;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.IIcon;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+public abstract class CMixinStickyPistonSides {
+	@Mixin(Block.class)
+	public interface IMixinBlock {
+		@Accessor
+		IIcon getBlockIcon();
+	}
+
+	@Mixin(BlockPistonBase.class)
+	public static abstract class MixinBlockPistonBase {
+		@Shadow
+		@Final
+		private boolean isSticky;
+
+		@Inject(method = "getPistonBaseIcon", at = @At("RETURN"), cancellable = true)
+		private static void allowStickyTextures(final String texture, final CallbackInfoReturnable<IIcon> cir) {
+			if ("piston_side_sticky".equals(texture)) {
+				cir.setReturnValue(((IMixinBlock) Blocks.sticky_piston).getBlockIcon());
+			}
+		}
+
+		@ModifyArg(method = "registerIcons", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/IIconRegister;registerIcon(Ljava/lang/String;)Lnet/minecraft/util/IIcon;", ordinal = 0))
+		private String makeSticky(final String texture) {
+			return this.isSticky && "piston_side".equals(texture) ? "tomrum:piston_side_sticky" : texture;
+		}
+	}
+
+	@Mixin(BlockPistonExtension.class)
+	public static abstract class MixinBlockPistonExtension {
+		@Inject(method = "getIcon", at = @At("RETURN"), cancellable = true)
+		private void slimeTheExtension(final int side, final int meta, final CallbackInfoReturnable<IIcon> cir) {
+			if ((meta & 8) != 0 && "piston_side".equals(cir.getReturnValue().getIconName())) {
+				cir.setReturnValue(BlockPistonBase.getPistonBaseIcon("piston_top_sticky"));
+			}
+		}
+	}
+}
